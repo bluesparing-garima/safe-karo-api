d9e690935a37cf19119ef80504919f95b90dedeb
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-06-29 11:40:42 +0530

    password decryption.

--- controller/adminController/userProfileController.js
+++ controller/adminController/userProfileController.js
@@ -2,18 +2,6 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import UserProfileModel from "../../models/adminModels/userProfileSchema.js";
 import UserModel from "../../models/userSchema.js";
-import crypto from "crypto";
-
-const algorithm = "aes-256-ctr";
-const secretKey = crypto.randomBytes(32);
-const iv = crypto.randomBytes(16);
-
-const encrypt = (text) => {
-  const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
-  const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-  return `${iv.toString("hex")}:${encrypted.toString("hex")}`;
-};
-
 
 // Function to generate Partner ID
 const generatePartnerId = async () => {
@@ -92,9 +80,7 @@ export const createUserProfile = async (req, res) => {
     }
 
     const existingUserInUserModel = await UserModel.findOne({ email });
-    const existingUserInUserProfileModel = await UserProfileModel.findOne({
-      email,
-    });
+    const existingUserInUserProfileModel = await UserProfileModel.findOne({ email });
 
     if (existingUserInUserModel || existingUserInUserProfileModel) {
       return res.status(400).json({
@@ -105,7 +91,6 @@ export const createUserProfile = async (req, res) => {
     const partnerId = await generatePartnerId();
 
     const hashedPassword = await hashPassword(password);
-    const encryptedPassword = encrypt(password);
 
     const userProfile = new UserProfileModel({
       branchName,
@@ -129,13 +114,13 @@ export const createUserProfile = async (req, res) => {
       createdBy,
       isActive: isActive !== undefined ? isActive : true,
       partnerId,
-      originalPassword: password,
+      originalPassword: password, 
     });
-
+      
     const newUser = new UserModel({
       name: fullName,
       email,
-      password: hashedPassword,
+      password: hashedPassword, 
       phoneNumber,
       role,
       isActive: isActive !== undefined ? isActive : true,
@@ -168,15 +153,13 @@ export const checkEmailExists = async (req, res) => {
     }
 
     const existingUserInUserModel = await UserModel.findOne({ email });
-    const existingUserInUserProfileModel = await UserProfileModel.findOne({
-      email,
-    });
+    const existingUserInUserProfileModel = await UserProfileModel.findOne({ email });
 
     if (existingUserInUserModel || existingUserInUserProfileModel) {
       return res.status(200).json({
         message: "Email already exists",
         emailExists: true,
-        status: "success",
+        status: "success"
       });
     } else {
       return res.status(200).json({
@@ -245,8 +228,6 @@ export const getUserProfileById = async (req, res) => {
       return res.status(404).json({ message: "User profile not found" });
     }
 
-    console.log("Original Password:", userProfile.originalPassword);
-
     res.status(200).json({
       message: "User profile retrieved successfully",
       data: userProfile,
@@ -260,8 +241,7 @@ export const getUserProfileById = async (req, res) => {
   }
 };
 
-
-// update
+// update userProfile 
 export const updateUserProfile = async (req, res) => {
   try {
     const { password, ...rest } = req.body;
@@ -270,7 +250,7 @@ export const updateUserProfile = async (req, res) => {
     if (password) {
       const hashedPassword = await hashPassword(password);
       updatedData.password = hashedPassword;
-      updatedData.originalPassword = password; // Store the original password directly
+      updatedData.originalPassword = password;
     } else {
       const existingProfile = await UserProfileModel.findById(req.params.id);
       if (existingProfile) {
@@ -285,7 +265,7 @@ export const updateUserProfile = async (req, res) => {
     );
 
     const updatedUserData = { ...updatedData };
-    delete updatedUserData.partnerId;
+    delete updatedUserData.partnerId; // Remove partnerId from the data to prevent updating it
 
     const updatedUser = await UserModel.findOneAndUpdate(
       { email: updatedData.email },
@@ -299,7 +279,6 @@ export const updateUserProfile = async (req, res) => {
     res.status(200).json({
       message: "User profile updated successfully",
       data: updatedProfile,
-      originalPassword: updatedProfile.originalPassword,
     });
   } catch (error) {
     res.status(500).json({
@@ -308,6 +287,7 @@ export const updateUserProfile = async (req, res) => {
     });
   }
 };
+
 
 // Delete (deactivate) a user profile by ID
 export const deleteUserProfile = async (req, res) => {
