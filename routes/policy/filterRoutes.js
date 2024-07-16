0301450b7620d9c2dca0a89ca384cf22e2238e10
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-07-16 13:00:26 +0530

    update od and tp in the bases of dates.

--- routes/policy/filterRoutes.js
+++ routes/policy/filterRoutes.js
@@ -1,8 +1,9 @@
-import { getAllMatchingRecords, getPoliciesByDateRange } from "../../controller/policyController/filterController.js";
+import { getAllMatchingRecords, getPoliciesByDateRange,updateODTPByDateRange } from "../../controller/policyController/filterController.js";
 import express from "express";
 import logActivity from "../../middlewares/logActivity.js";
 
 const router = express.Router();
 router.get("/date-range",logActivity, getPoliciesByDateRange);
 router.get('/policies',logActivity,getAllMatchingRecords);
+router.put('/update-od-tp',logActivity, updateODTPByDateRange);
 export default router;
