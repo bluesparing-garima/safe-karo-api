99d1b93c50d3fc75165868181c428ebed3cd6938
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-07-17 15:57:06 +0530

    accounttable done.

--- routes/accountRoutes/accountRoute.js
+++ routes/accountRoutes/accountRoute.js
@@ -1,9 +1,28 @@
 import express from 'express';
-import { createAccount } from '../../controller/accountsController/accountController.js';
+import {
+  createAccount,
+  getAllAccountDetails,
+  getAccountById,
+  updateAccount,
+  deleteAccount
+} from '../../controller/accountsController/accountController.js';
 import logActivity from '../../middlewares/logActivity.js';
 
 const router = express.Router();
 
-router.post('/',logActivity,createAccount);
+// Create Account
+router.post('/', logActivity, createAccount);
+
+// Get All Account details
+router.get('/', logActivity, getAllAccountDetails);
+
+// Get Account by ID
+router.get('/:id', logActivity, getAccountById);
+
+// Update Account
+router.put('/:id', logActivity, updateAccount);
+
+// Delete Account
+router.delete('/:id', logActivity, deleteAccount);
 
 export default router;
