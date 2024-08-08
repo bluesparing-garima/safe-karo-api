77905a9ae78d428f06674910f175fd8e1d2ba5a2
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-08-08 15:01:50 +0530

    partner dashboard previous one.

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ app.use("/api/branches", branch);
 app.use("/api/dashboard", adminDashboard);
 
 // partner dashboard
-app.use("/api/partner-dashboard", partnerDashboardRoutes);
+app.use("/api/dashboard", partnerDashboardRoutes);
 
 // partner dashboard
 app.use("/api/booking-dashboard", bookingDashboardRoutes);
