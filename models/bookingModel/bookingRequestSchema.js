265a022df444e1b9090a7e22de3abee04d3d7515
bluesparing-garima <garima@bluesparing.com>
2024-08-14 11:09:58 +0530

    Add Timer column in lead and booking

--- models/bookingModel/bookingRequestSchema.js
+++ models/bookingModel/bookingRequestSchema.js
@@ -32,6 +32,7 @@ const bookingRequestSchema = new mongoose.Schema({
     bookingStatus:{type:String,trim:true},
     bookingAcceptedBy:{type:String,trim:true},
     leadId:{type:String,trim:true},
+    timer: { type: String },
     createdBy: { type: String, trim: true },
     createdOn: { type: Date, default: Date.now },
     updatedBy: { type: String, default: null },
