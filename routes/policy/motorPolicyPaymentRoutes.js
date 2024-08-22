8d6c07ea35c2da9053afc3e260a4f870e5e47a74
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-08-22 16:13:31 +0530

    api to get all details of payOutStatus = paid by date range and partnerId.

--- routes/policy/motorPolicyPaymentRoutes.js
+++ routes/policy/motorPolicyPaymentRoutes.js
@@ -7,7 +7,8 @@ import {
   updateMotorPolicyPayment,
   deleteMotorPolicyPayment,
   policyStatusManage,
-  getUnPaidAndPartialPaidPayments
+  getUnPaidAndPartialPaidPayments,
+  getPaidPayments
 
 } from '../../controller/policyController/motorPolicyPaymentController.js';
 
@@ -15,6 +16,7 @@ const router = express.Router();
 router.post('/', logActivity, createMotorPolicyPayment);
 router.get('/', logActivity, getAllMotorPolicyPayments);
 router.get('/partner-id/status', logActivity, getUnPaidAndPartialPaidPayments);
+router.get('/partner-id/paid-status', logActivity, getPaidPayments);
 router.get('/:policyId', logActivity, getMotorPolicyPaymentByPolicyId);
 router.put('/status-manage',logActivity,policyStatusManage);
 router.put('/:policyId', logActivity, updateMotorPolicyPayment);
