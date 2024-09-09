c70840e7bda7b522e45d95c4ef15bbcf157a9e64
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-09-09 15:56:57 +0530

    token encryption decryption done using crypto-js

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,5 +1,11 @@
 import jwt from "jsonwebtoken";
 import UserModel from "../models/userSchema.js";
+import CryptoJS from "crypto-js";
+
+const decryptData = (ciphertext) => {
+  const bytes = CryptoJS.AES.decrypt(ciphertext, process.env.SECRET_KEY);
+  return bytes.toString(CryptoJS.enc.Utf8); 
+};
 
 const checkUserAuth = async (req, res, next) => {
   let token;
@@ -9,12 +15,17 @@ const checkUserAuth = async (req, res, next) => {
     try {
       token = authorization.split(" ")[1];
 
-      const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY);
+      const decryptedToken = decryptData(token);
+      if (!decryptedToken) {
+        return res.status(401).json({ status: "failed", message: "Invalid Token" });
+      }
+
+      const { userID } = jwt.verify(decryptedToken, process.env.JWT_SECRET_KEY);
 
       req.user = await UserModel.findById(userID).select("-password");
 
       if (req.user) {
-        next();
+        next(); 
       } else {
         res.status(401).json({ status: "failed", message: "Unauthorized User" });
       }
