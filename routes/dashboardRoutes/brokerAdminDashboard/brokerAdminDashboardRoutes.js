81d96583e0797402d9d65fae5416daf4b3f5f6a2
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-09-19 12:04:07 +0530

    changed payOut to payIn.

--- routes/dashboardRoutes/brokerAdminDashboard/brokerAdminDashboardRoutes.js
+++ routes/dashboardRoutes/brokerAdminDashboard/brokerAdminDashboardRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { getAllBrokersWithPayInCommissionAndDateFilter,getAllBrokersWithPayInCommission,getPayOutCommissionByBrokerAndCompanyWithDateFilter,getPayOutCommissionByBrokerAndCompany } from "../../../controller/dashboard/brokerAdminDashboard/brokerAdminDashboardController.js";
+import { getAllBrokersWithPayInCommissionAndDateFilter,getAllBrokersWithPayInCommission,getPayInCommissionByBrokerAndCompanyWithDateFilter,getPayInCommissionByBrokerAndCompany } from "../../../controller/dashboard/brokerAdminDashboard/brokerAdminDashboardController.js";
 import logActivity from "../../../middlewares/logActivity.js";
 const router = express.Router();
 
 // Route for counting users by role
 router.get("/broker-date-filter", logActivity, getAllBrokersWithPayInCommissionAndDateFilter);
 router.get("/", logActivity, getAllBrokersWithPayInCommission);
-router.get("/company-name/broker-date-filter", logActivity, getPayOutCommissionByBrokerAndCompanyWithDateFilter);
-router.get("/company-name", logActivity, getPayOutCommissionByBrokerAndCompany);
+router.get("/company-name/broker-date-filter", logActivity, getPayInCommissionByBrokerAndCompanyWithDateFilter);
+router.get("/company-name", logActivity, getPayInCommissionByBrokerAndCompany);
 export default router;
