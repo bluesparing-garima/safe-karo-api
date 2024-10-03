b863c7743ffc7aea2028d3cea54dbc1b031542ec
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-10-03 16:09:34 +0530

    pdf reader for TP of compnay tata is completed.

--- routes/pdfReaderRoute.js
+++ routes/pdfReaderRoute.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import { handleFileUpload } from '../middlewares/pdfMiddleware.js';
-import { TataPDFParsing } from '../controller/pdfReaderController.js';
+import express from "express";
+import { handleFileUpload } from "../middlewares/pdfMiddleware.js";
+import {
+  TataPDFParsing,
+  TataPDFParsingTP,
+} from "../controller/pdfReaderController.js";
 import logActivity from "../middlewares/logActivity.js";
 
 const router = express.Router();
 
-router.post('/upload',logActivity, handleFileUpload, TataPDFParsing);
-
+router.post("/upload", logActivity, handleFileUpload, TataPDFParsing);
+router.post("/tp-upload", logActivity, handleFileUpload, TataPDFParsingTP);
 export default router;
