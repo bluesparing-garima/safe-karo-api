4658ab8791c71e3e4743524a884649cfbf4ec26a
nikhilbluesparing <nikhilpaidy@bluesparing.com>
2024-10-17 16:26:24 +0530

    payOutCommission changed.

--- controller/dashboard/partnerDashboardController.js
+++ controller/dashboard/partnerDashboardController.js
@@ -179,7 +179,7 @@ export const getPartnerDashboardCount = async (req, res) => {
           payOutPaymentStatus: "Paid",
         },
       },
-      { $group: { _id: null, totalPayOutAmount: { $sum: "$payOutAmount" } } },
+      { $group: { _id: null, totalPayOutAmount: { $sum: "$payOutCommission" } } },
     ]);
     const monthlyPaidAmount =
       monthlyPaidAmountAggregate.length > 0
@@ -189,7 +189,7 @@ export const getPartnerDashboardCount = async (req, res) => {
     // Calculate Total Paid Amount
     const totalPaidAmountAggregate = await MotorPolicyPaymentModel.aggregate([
       { $match: { partnerId, isActive: true, payOutPaymentStatus: "Paid" } },
-      { $group: { _id: null, totalPayOutAmount: { $sum: "$payOutAmount" } } },
+      { $group: { _id: null, totalPayOutAmount: { $sum: "$payOutCommission" } } },
     ]);
     const totalPaidAmount =
       totalPaidAmountAggregate.length > 0
